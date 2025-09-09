a3d39f330db5d6f84b3aefcf73fa492bd2a9ba08
Emerson Pereira da Silva <pereiradasilva.emerson@gmail.com>
2025-09-09 20:05:31 -0300

    Initial project

---
+++ src/main/java/br/com/fiap/challenge/tech_challenge_fase_01/TechChallengeFase01Application.java
@@ -0,0 +1,13 @@
+package br.com.fiap.challenge.tech_challenge_fase_01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechChallengeFase01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechChallengeFase01Application.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fiap/challenge/tech_challenge_fase_01/TechChallengeFase01ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.challenge.tech_challenge_fase_01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechChallengeFase01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
