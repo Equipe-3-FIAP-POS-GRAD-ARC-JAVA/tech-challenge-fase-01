2b6b170f43943384a531aa6704340f1c0106f450
viniciuspadovam <padovam3373@gmail.com>
2025-09-17 09:01:40 -0300

    feat: add global exception handler

---
+++ src/main/java/br/com/fiap/challenge/tech_challenge_fase_01/infrastructure/GlobalExceptionHandler.java
@@ -0,0 +1,40 @@
+package br.com.fiap.challenge.tech_challenge_fase_01.infrastructure;
+
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ProblemDetail;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import br.com.fiap.challenge.tech_challenge_fase_01.infrastructure.exception.NotFoundException;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(NotFoundException.class)
+    public ProblemDetail handleNotFoundException(NotFoundException ex) {
+        return this.buildProblemDetail(ex.getStatusCode(), ex.getMessage());
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ProblemDetail handleIllegalArgumentException(IllegalArgumentException ex) {
+        return this.buildProblemDetail(HttpStatus.BAD_REQUEST, ex.getMessage());
+    }
+
+    private ProblemDetail buildProblemDetail(HttpStatusCode statusCode, String message) {
+        var problemDetail = ProblemDetail.forStatusAndDetail(statusCode, message);
+        problemDetail.setProperty("timestamp", this.getDateTimeFormatted());
+        return problemDetail;
+    }
+
+    private String getDateTimeFormatted() {
+        var now = ZonedDateTime.now(ZoneId.of("America/Sao_Paulo"));
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss");
+        return formatter.format(now);
+    }
+
+}
