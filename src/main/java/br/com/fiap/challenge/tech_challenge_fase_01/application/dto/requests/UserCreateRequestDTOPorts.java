790a84d95932859f4c4e6fbc34754ac53a05e126
Emerson Pereira da Silva <pereiradasilva.emerson@gmail.com>
2025-10-04 19:21:02 -0300

    Add toDomain

--- src/main/java/br/com/fiap/challenge/tech_challenge_fase_01/application/dto/requests/UserCreateRequestDTOPorts.java
+++ src/main/java/br/com/fiap/challenge/tech_challenge_fase_01/application/dto/requests/UserCreateRequestDTOPorts.java
@@ -1,9 +1,20 @@
 package br.com.fiap.challenge.tech_challenge_fase_01.application.dto.requests;
 
+import br.com.fiap.challenge.tech_challenge_fase_01.application.domain.user.UserDomain;
+
 public record UserCreateRequestDTOPorts(
-        String name,
-        String email,
-        String login,
-        String password) {
+                String name,
+                String email,
+                String login,
+                String password) {
+
+        public UserDomain toDomain(UserCreateRequestDTOPorts userCreateRequestDTO) {
+                return UserDomain.builder()
+                                .name(userCreateRequestDTO.name())
+                                .email(userCreateRequestDTO.email())
+                                .login(userCreateRequestDTO.login())
+                                .password(userCreateRequestDTO.password())
+                                .build();
+        }
 
 }
--- src/main/java/br/com/fiap/challenge/tech_challenge_fase_01/application/dto/requests/UserUpdateRequestDTOPorts.java
+++ src/main/java/br/com/fiap/challenge/tech_challenge_fase_01/application/dto/requests/UserUpdateRequestDTOPorts.java
@@ -1,9 +1,28 @@
 package br.com.fiap.challenge.tech_challenge_fase_01.application.dto.requests;
 
+import br.com.fiap.challenge.tech_challenge_fase_01.application.domain.user.UserDomain;
+
 public record UserUpdateRequestDTOPorts(
-        String id,
-        String name,
-        String email,
-        String login) {
+                String id,
+                String name,
+                String email,
+                String login) {
+
+        public UserDomain toDomain(UserCreateRequestDTOPorts userCreateRequestDTO) {
+                return UserDomain.builder()
+                                .name(userCreateRequestDTO.name())
+                                .email(userCreateRequestDTO.email())
+                                .login(userCreateRequestDTO.login())
+                                .password(userCreateRequestDTO.password())
+                                .build();
+        }
+
+        public UserDomain toDomain(UserUpdateRequestDTOPorts userCreateRequestDTO) {
+                return UserDomain.builder()
+                                .name(userCreateRequestDTO.name())
+                                .email(userCreateRequestDTO.email())
+                                .login(userCreateRequestDTO.login())
+                                .build();
+        }
 
 }
