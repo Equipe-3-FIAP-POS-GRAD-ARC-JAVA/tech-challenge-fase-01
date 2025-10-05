e14a81cf3687c4222ee5ba852e1116ef2a10141e
Emerson Pereira da Silva <pereiradasilva.emerson@gmail.com>
2025-10-04 23:28:43 -0300

    Deleted Files or Moved

--- src/main/java/br/com/fiap/challenge/tech_challenge_fase_01/adapters/inbound/requests/LoginRequest.java
+++
@@ -1,9 +0,0 @@
-package br.com.fiap.challenge.tech_challenge_fase_01.adapters.inbound.requests;
-
-import jakarta.validation.constraints.NotBlank;
-
-public record LoginRequest(
-        @NotBlank String login,
-        @NotBlank String password
-) {
-}
--- src/main/java/br/com/fiap/challenge/tech_challenge_fase_01/adapters/outbound/entities/enumx/RolesEnum.java
+++
@@ -1,7 +0,0 @@
-package br.com.fiap.challenge.tech_challenge_fase_01.adapters.outbound.entities.enumx;
-
-public enum RolesEnum {
-
-    OWNER, CLIENT, ADMIN
-
-}
