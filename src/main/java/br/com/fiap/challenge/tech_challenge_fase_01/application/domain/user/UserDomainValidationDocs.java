9ef1ba1e1ca6e2054cba4e2b5438231b85d30bd6
Emerson Pereira da Silva <pereiradasilva.emerson@gmail.com>
2025-10-11 20:05:51 -0300

    Remove unused UserDomainValidationDocs class

--- src/main/java/br/com/fiap/challenge/tech_challenge_fase_01/application/domain/user/UserDomainValidationDocs.java
+++
@@ -1,7 +0,0 @@
-package br.com.fiap.challenge.tech_challenge_fase_01.application.domain.user;
-
-public final class UserDomainValidationDocs {
-    private UserDomainValidationDocs() {
-        throw new UnsupportedOperationException("Documentation class - should not be instantiated");
-    }
-}
