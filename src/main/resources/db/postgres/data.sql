d1144512e296a6f36a9be05fc081eabcc51a4987
Emerson Pereira da Silva <pereiradasilva.emerson@gmail.com>
2025-10-15 18:08:15 -0300

    Add new user 'Emerson Silva' with admin role and associated address

--- src/main/resources/db/postgres/data.sql
+++ src/main/resources/db/postgres/data.sql
@@ -21,10 +21,11 @@ VALUES ('a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a14', 'Ana Souza', 'ana.souza@example.
 INSERT INTO "users" (id, name, email, login, password, created_at, updated_at, roles, is_active)
 VALUES ('a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a15', 'Carlos Oliveira', 'carlos.oliveira@example.com', 'carlosol', '$2a$10$eNG0/QQuDXqfEIlvDBfDb.hkiy22teXIBEMLlug.AZnZTqDE4UAUi', NOW(), NOW(), '{OWNER}', true) ON CONFLICT (email) DO NOTHING;
 
--- Adicionar o usuário joao.silva com senha123
 INSERT INTO "users" (id, name, email, login, password, created_at, updated_at, roles, is_active)
 VALUES ('a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a17', 'João Silva', 'joao.silva@example.com', 'joao.silva', '$2a$10$eNG0/QQuDXqfEIlvDBfDb.hkiy22teXIBEMLlug.AZnZTqDE4UAUi', NOW(), NOW(), '{CLIENT}', true) ON CONFLICT (email) DO NOTHING;
 
+INSERT INTO "users" (id, name, email, login, password, created_at, updated_at, roles, is_active)
+VALUES ('a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a16', 'Emerson Silva', 'emerson.silva@example.com', 'emerson.silva', '$2a$10$eNG0/QQuDXqfEIlvDBfDb.hkiy22teXIBEMLlug.AZnZTqDE4UAUi', NOW(), NOW(), '{ADMIN}', true) ON CONFLICT (email) DO NOTHING;
 
 -- ADDRESSES (referenciam os usuários já criados)
 INSERT INTO address (id, user_id, street, number, city, created_at, updated_at)
@@ -41,3 +42,6 @@ VALUES (gen_random_uuid(), 'a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a14', 'Av. Atlânti
 
 INSERT INTO address (id, user_id, street, number, city, created_at, updated_at)
 VALUES (gen_random_uuid(), 'a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a15', 'Rua das Flores', '45B', 'Belo Horizonte', NOW(), NOW()) ON CONFLICT (id) DO NOTHING;
+
+INSERT INTO address (id, user_id, street, number, city, created_at, updated_at)
+VALUES (gen_random_uuid(), 'a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a16', 'Av. Paulista', '1000', 'São Paulo', NOW(), NOW()) ON CONFLICT (id) DO NOTHING;
